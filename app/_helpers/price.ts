edb83f713962bcc69d34bef97cf940655e0ace77
famgz <famgz@proton.me>
2024-05-01 01:59:43 -0300

    feat: currency helper functions

---
+++ app/_helpers/price.ts
@@ -0,0 +1,20 @@
+import { Product } from "@prisma/client";
+
+export function calculateProductTotalPrice(product: Product): number {
+  const price = Number(product.price);
+  let discount = Number(product.discountPercentage);
+
+  if (product.discountPercentage === 0) {
+    return price;
+  }
+  discount = price * (product.discountPercentage / 100);
+  return Number(price) - discount;
+}
+
+export function formatPrice(price: number) {
+  return Intl.NumberFormat("pt-br", {
+    style: "currency",
+    currency: "BRL",
+    minimumFractionDigits: 2,
+  }).format(price);
+}
