48cf8e94bf4fcce1c0dcad0327666fec85e9e43a
famgz <famgz@proton.me>
2024-05-02 01:07:30 -0300

    feat: header logo links to home page

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <div className="flex justify-between px-5 pt-6">
-      <Image src="/logo.svg" alt="Food Hub" height={30} width={100} />
+      <Link href="/">
+        <Image src="/logo.svg" alt="Food Hub" height={30} width={100} />
+      </Link>
       <Button
         size="icon"
         variant="outline"
